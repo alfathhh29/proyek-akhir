02cde9ca0021a20c7034f70a30c4b64921512feb
alfathhh29 <201014012@fellow.lpkia.ac.id>
2022-11-09 16:13:18 +0700

    form

--- resources/views/person/create.blade.php
+++ resources/views/person/create.blade.php
@@ -10,25 +10,25 @@
             </ul>
         </div>
     @endif
-@endsection
-<div> class="card my-3">
-    <div> class="card-header">
+<div class="card my-3">
+    <div class="card-header">
         Form Tambah Data
     </div>
-    <div> class="card-body">
+    <div class="card-body">
         <form action="{{ route('person.store') }}" method="POST">
             @csrf
-            <div> class="form-group">
+            <div class="form-group">
                 <label for="name">Nama</label>
-                <input type="text">" name="name" placeholder="masukan Nama A"
+                <input type="text" name="name" class="form-controller" placeholder="masukan Nama A">
             </div>
-            <div> class="form-group">
+            <div class="form-group">
                 <label for="email">Email</label>
-                <input type="email"> name="email" class="form-controller" placeholder="nama@email.com">
+                <input type="email" name="email" class="form-controller" placeholder="nama@email.com">
             </div>
-            <div> class="form-group">
+            <div class="form-group">
                 <input type="submit" value="Simpan" class="btn btm-success">
             </div>
         </form>
     </div>
 </div>
+@endsection
